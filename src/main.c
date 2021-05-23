51c87aa9bb5695c85014b02c64f76feab975a8ee
jaq-cat <jaq.cat@protonmail.ch>
2021-05-23 11:47:08 -0300

    lift stuff

--- src/main.c
+++ src/main.c
@@ -11,9 +11,10 @@ int main() {
     int position;
     PhysicsObject mig15 = {
         0, // initial velocity (m/s)
-        0, // initial angle (degrees)
+        12, // initial angle (degrees)
         4500, // mass (kg)
         (26.5)*1000, // thrust (N)
+        1, // lift constant
         0.2, // drag coefficient at 0°
         0.9, // drag coefficient at 90°
         3, // area at 0°
@@ -21,15 +22,24 @@ int main() {
     };
 
     initscr();
+    cbreak();
     noecho();
+    keypad(stdscr, true);
 
-    while (getch() != 'q') {
+    int input;
+    while ((input = getch()) != 'q') {
         clear();
-        mvprintw(0, 0, "Thrust:       %f", mig15.thrust);
-        mvprintw(1, 0, "Drag:         %f", drag(&mig15));
-        mvprintw(2, 0, "Acceleration: %f", acc(&mig15));
-        mvprintw(3, 0, "Velocity:     %f", mig15.velocity);
-        mvprintw(4, 0, "Position:     %d", position);
+        if (input == KEY_UP)
+            mig15.angle += 0.5;
+        else if (input == KEY_DOWN)
+            mig15.angle -= 0.5;
+        mvprintw(0, 0, "Thrust:           %f", mig15.thrust);
+        mvprintw(1, 0, "Drag:             %f", drag(&mig15));
+        mvprintw(2, 0, "Acceleration:     %f", acc(&mig15));
+        mvprintw(3, 0, "Velocity:         %f", mig15.velocity);
+        mvprintw(4, 0, "Position:         %d", position);
+        mvprintw(5, 0, "Angle:            %f", mig15.angle);
+        mvprintw(6, 0, "Lift Coefficient: %f", get_lift_coeff(&mig15));
         mig15.velocity += acc(&mig15);
         position += mig15.velocity;
 
--- src/physics.h
+++ src/physics.h
@@ -6,25 +6,43 @@ typedef struct {
     float angle;
     float mass;
     float thrust;
+    float lift_const;
     float cd_min;
     float cd_max;
     float area_min;
     float area_max;
 } PhysicsObject;
 
-float equiv(float maxv, float minv, float angle) {
-    while (angle >= 90)
-        angle -= 90;
+float equiv(float minv, float maxv, float angle) {
     float a = (maxv - minv) / 90;
     return angle * a + minv;
 }
 
+float get_lift_coeff(PhysicsObject *obj) {
+    float angle = obj->angle;
+    float lift = 0;
+    while (angle >= 180)
+        angle -= 180;
+    if (angle >= 0 && angle < 15) lift = equiv(0, 1.4, angle);
+    if (angle >= 15 && angle < 50) lift = equiv(1.4, 1, angle);
+    if (angle >= 50 && angle < 100) lift = equiv(1, 0, angle);
+    if (angle >= 100 && angle < 150) lift = equiv(0, -1, angle);
+    if (angle >= 150 && angle < 180) lift = equiv(-1, 0, angle);
+    return lift;
+}
+
 float get_drag_coeff(PhysicsObject *obj) {
-    return equiv(obj->cd_max, obj->cd_min, obj->angle);
+    float angle = obj->angle;
+    while (angle >= 90)
+        angle -= 90;
+    return equiv(obj->cd_min, obj->cd_max, angle);
 }
 
 float get_frontal_area(PhysicsObject *obj) {
-    return equiv(obj->area_max, obj->area_min, obj->angle);
+    float angle = obj->angle;
+    while (angle >= 90)
+        angle -= 90;
+    return equiv(obj->area_min, obj->area_max, angle);
 }
 
 float drag(PhysicsObject *obj) {
